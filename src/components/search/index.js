fd1df3aa70d5550bdf23d44c09f546276cfd2a05
Jennifer Scheuren <jennifer.scheuren@sonicfoundry.com>
2023-07-20 16:14:39 -0500

    all files

---
+++ src/components/search/index.js
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import { useNavigate, useSearchParams, createSearchParams } from 'react-router-dom';
+
+
+
+const Search = () => {
+
+  // get navigate function
+  const navigate = useNavigate();
+ 
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    }
+
+    // use createSearchParams 
+    const query = createSearchParams(searchQuery);
+
+
+
+
+    navigate(`/search?${query}`)
+    // imperatively redirect with useNavigate() returned function
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
